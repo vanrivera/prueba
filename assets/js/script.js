e04446d3658face2c0b073df0287e352dc07e217
Vania <vaniakatherina.r@gmail.com>
2020-06-23 23:13:57 -0400

    first commit prueba

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover()
+})
+
+
+
+/* var boton = $('button');
+console.log(boton);
+
+$('button').click(function (){
+    //alert("click en boton");
+    $('p').toggle();
+}) */
+
+/* 
+$("button").click(function() {
+    $( "p" ).toggle();
+}); */
+
+/*     console.log("desde jquery")
+    alert("desde jquery");
+    var x = 100;
+    var z = -35.67;
+    var suma = x / z;
+    console.log(suma); */
